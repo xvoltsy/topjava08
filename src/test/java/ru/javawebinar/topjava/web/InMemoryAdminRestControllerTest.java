cc89c4b6499701a1d296541b56df5b52417f4705
xvoltsy email = vladimir.tsypanov@gmail.com[core] autocrlf = true <v.tsipanov@hotmail.com>
2016-10-05 23:59:29 +0300

    3_0_fix_servlet

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.junit.*;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
