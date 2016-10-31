ab1c216740af66ae4fc90ddd68d7da9ffe4c537b
xvoltsy email = vladimir.tsypanov@gmail.com[core] autocrlf = true <v.tsipanov@hotmail.com>
2016-10-31 15:55:45 +0200

    HW05

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractBaseTest.java
@@ -0,0 +1,55 @@
+package ru.javawebinar.topjava.service;
+
+
+import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.Profiles;
+
+import java.util.concurrent.TimeUnit;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles(Profiles.ACTIVE_DB)
+public abstract class AbstractBaseTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractBaseTest.class);
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private static StringBuffer results = new StringBuffer();
+
+    @AfterClass
+    public static void printResult() {
+        System.out.printf("%nTest             Duration, ms%n");
+        System.out.println("-----------------------------");
+        System.out.println(results);
+        System.out.printf("-----------------------------%n%n");
+    }
+
+    @Rule
+    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String result = String.format("%-20s %8d", description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+            results.append(result).append('\n');
+            LOG.info(result + " ms\n");
+        }
+    };
+}
