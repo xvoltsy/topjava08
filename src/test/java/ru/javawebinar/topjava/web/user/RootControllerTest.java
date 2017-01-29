910fb562d8e815020fbba3bae91220f5cc399402
xvoltsy email = vladimir.tsypanov@gmail.com[core] autocrlf = true <v.tsipanov@hotmail.com>
2017-01-29 13:27:50 +0200

    HW07

--- src/test/java/ru/javawebinar/topjava/web/user/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/RootControllerTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 /**
@@ -28,6 +29,24 @@ public class RootControllerTest extends AbstractControllerTest {
                         allOf(
                                 hasProperty("id", is(START_SEQ)),
                                 hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+
+    @Test
+    public void testMeals() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id"),
+                                hasProperty("dateTime"),
+                                hasProperty("description"),
+                                hasProperty("calories")
                         )
                 )));
     }
