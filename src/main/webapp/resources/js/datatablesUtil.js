243449439a55029cb1b7dcc9a8ec5cb6c80356a5
vladimir <vladimir.tsypanov@gmail.com>
2017-12-23 20:08:35 +0200

    9 06 validation

--- src/main/webapp/resources/js/datatablesUtil.js
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -73,7 +73,7 @@ function successNoty(text) {
 function failNoty(jqXHR) {
     closeNoty();
     failedNote = noty({
-        text: 'Error status: ' + jqXHR.status,
+        text: "<span class='glyphicon glyphicon-exclamation-sign'></span> &nbsp;Error status: " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
         type: 'error',
         layout: 'bottomRight'
     });
