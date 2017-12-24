2f1d2b6aae8b3702b7e3b275b560b484cf1ea396
vladimir <vladimir.tsypanov@gmail.com>
2017-12-24 14:06:27 +0200

    9_11_auth_via_user_service

--- src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
+++ src/main/java/ru/javawebinar/topjava/AuthorizedUser.java
@@ -1,14 +1,45 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import ru.javawebinar.topjava.model.BaseEntity;
+import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.UserUtil;
+import ru.javawebinar.topjava.model.User;
+
+import static ru.javawebinar.topjava.util.UserUtil.DEFAULT_CALORIES_PER_DAY;
+import static java.util.Objects.requireNonNull;
 
 /**
  * GKislin
  * 06.03.2015.
  */
-public class AuthorizedUser {
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User{
     public static int id = BaseEntity.START_SEQ;
+
+    private static final long serialVersionUID = 1L;
+
+    private final UserTo userTo;
+
+    public AuthorizedUser(User user) {
+        super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
+        this.userTo = UserUtil.asTo(user);
+    }
+
+    public static AuthorizedUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        return (principal instanceof AuthorizedUser) ? (AuthorizedUser) principal : null;
+    }
+
+    public static AuthorizedUser get() {
+        AuthorizedUser user = safeGet();
+        requireNonNull(user, "No authorized user found");
+        return user;
+    }
 
     public static int id() {
         return id;
@@ -19,6 +50,11 @@ public class AuthorizedUser {
     }
 
     public static int getCaloriesPerDay() {
-        return UserUtil.DEFAULT_CALORIES_PER_DAY;
+        return DEFAULT_CALORIES_PER_DAY;
+    }
+
+    @Override
+    public String toString() {
+        return userTo.toString();
     }
 }
