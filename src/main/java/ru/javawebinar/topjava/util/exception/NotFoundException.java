8cc49985dedc92e4bce6f8ac5e05e0f5d2e17273
xvoltsy <vladimir.tsypanov@gmail.com>
2018-10-25 17:41:40 +0300

    11_0_2_fix

--- src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
@@ -1,12 +1,14 @@
 package ru.javawebinar.topjava.util.exception;
 
 
+import javax.validation.constraints.NotNull;
+
 /**
  * User: gkislin
  * Date: 19.08.2014
  */
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
+    public NotFoundException(@NotNull String message) {
         super(message);
     }
 }
